6ce898b665304f8bcb098f8ff57edee613541509
U-Michal-Komputer\Michal <Michal@Michal-Komputer.(none)>
2010-09-11 05:04:42 +0200

    part1 of the Simple Canvas game Tutorial

---
+++ part1/game.js
@@ -0,0 +1,56 @@
+var width = 320, 
+	height = 500,
+	gLoop,
+	c = document.getElementById('c'), 
+	ctx = c.getContext('2d');
+			
+	c.width = width;
+	c.height = height;
+
+
+var clear = function(){
+	ctx.fillStyle = '#d0e7f9';
+	ctx.clearRect(0, 0, width, height);
+	ctx.beginPath();
+	ctx.rect(0, 0, width, height);
+	ctx.closePath();
+	ctx.fill();
+}
+
+var howManyCircles = 10, circles = [];
+
+for (var i = 0; i < howManyCircles; i++) 
+	circles.push([Math.random() * width, Math.random() * height, Math.random() * 100, Math.random() / 2]);
+
+var DrawCircles = function(){
+	for (var i = 0; i < howManyCircles; i++) {
+		ctx.fillStyle = 'rgba(255, 255, 255, ' + circles[i][3] + ')';
+		ctx.beginPath();
+		ctx.arc(circles[i][0], circles[i][1], circles[i][2], 0, Math.PI * 2, true);
+		ctx.closePath();
+		ctx.fill();
+	}
+};
+
+var MoveCircles = function(e){
+	for (var i = 0; i < howManyCircles; i++) {
+		if (circles[i][1] - circles[i][2] > height) {
+			circles[i][0] = Math.random() * width;
+			circles[i][2] = Math.random() * 100;
+			circles[i][1] = 0 - circles[i][2];
+			circles[i][3] = Math.random() / 2;
+		}
+		else {
+			circles[i][1] += e;
+		}
+	}
+};
+var GameLoop = function(){
+	clear();
+	MoveCircles(5);
+	DrawCircles();
+	
+	gLoop = setTimeout(GameLoop, 1000 / 50);
+}
+
+GameLoop();
